7ce9f1efc90f03c3bef7f48035fc8fb19ca4604f
Tahir-DA-Ds <tahir.adeleye@gmail.com>
2023-10-30 00:07:45 +0100

    module fix

--- server/controller/admin.js
+++ server/controller/admin.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const User = require("../model/User");
+const User = require('../model/USer')
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const jwtSecret = process.env.JWT_SECRET;
